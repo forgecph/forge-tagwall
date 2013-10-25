f484b9b74647059fa7b709d6b298253099e0294f
Casper Klenz-Kitenge <hi@cabgfx.com>
2013-10-25 08:06:51 +0200

    Feed slides w/ twitter, instagram and facebook

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 gem "sinatra"
 gem "twitter"
 gem "instagram"
+gem "fb_graph"
 
 group :development do
   gem "powder"
--- show.rb
+++ show.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "twitter"
 require "instagram"
+require "fb_graph"
 
 Twitter.configure do |config|
   config.consumer_key = ENV['TWITTER_CONSUMER_KEY']
@@ -14,10 +15,12 @@ Instagram.configure do |config|
   config.client_secret = ENV['INSTAGRAM_CLIENT_SECRET']
 end
 
+facebook = FbGraph::Application.new(ENV['FACEBOOK_APP_ID'], :secret => ENV['FACEBOOK_APP_SECRET'])
+
 get '/' do
   @tweets = Twitter.search('#forgecph').results
-  # @feed.sort(&:source) TODO
   @grams = Instagram.tag_recent_media("forgecph")
+  @faces = FbGraph::Searchable.search('#forgecph', access_token: facebook.get_access_token)
   
   erb :wall
 end
